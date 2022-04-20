3b7c494dab048ab35716ad4d8a58ef2d3fb3ab6e
Nurpatsha <90358219+nurlrr@users.noreply.github.com>
2022-04-20 22:52:53 +0600

    Update DatabaseTest.java

--- src/cs520/hw5/DatabaseTest.java
+++ src/cs520/hw5/DatabaseTest.java
@@ -2,7 +2,54 @@ package cs520.hw5;
 
 public class DatabaseTest {
 
-	// Copy main() and createPlayer() from TeamTest (Assignment 4) here and
-	// make the modifications listed in the Assignment 5 document.
+    public static void main(String[] args) throws DatabaseException {
+        Scanner scanner = null;
 
+        try {
+            File file = new File("team.txt");
+            scanner = new Scanner(file);
+            Collection<Player> players = new LinkedHashSet<>();
+
+            while (scanner.hasNextLine()) {
+                String number = scanner.nextLine();
+                String name = scanner.nextLine();
+                String position = scanner.nextLine();
+                String year = scanner.nextLine();
+                scanner.nextLine();
+
+                Player eachPlayer = createPlayer(number, name, position, year);
+                players.add(eachPlayer);
+            }
+
+            Database.insertPlayers(players);
+
+            Map<String, Player> selectedPlayers =  Database.selectPlayers();
+
+            Iterator<String> it = selectedPlayers.keySet().iterator();
+            while(it.hasNext()) {
+                String key = it.next();
+                System.out.println(key + ": " + selectedPlayers.get(key));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            throw new DatabaseException();
+        } finally {
+            scanner.close();
+        }
+    }
+
+    static Player createPlayer(String number, String name, String position, String year) throws SQLException {
+
+        int numberInt=Integer.parseInt(number);
+
+        Player player = new Player(name);
+        player.setNumber(numberInt);
+        player.setPosition(position);
+        player.setYear(year);
+
+        return player;    
+    }
 }
