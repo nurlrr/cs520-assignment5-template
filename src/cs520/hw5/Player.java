9d1f1f10a2aec3c312259b529e6f9bdf5fbdd3af
Nurpatsha <90358219+nurlrr@users.noreply.github.com>
2022-04-20 22:50:53 +0600

    Update Player.java

--- src/cs520/hw5/Player.java
+++ src/cs520/hw5/Player.java
@@ -1,7 +1,55 @@
 package cs520.hw5;
 
-// Overwrite this file with your Player.java file from Assignment 4
-// and make the changes indicated in the Assignment 5 document.
+import java.util.Random;
 
 public class Player {
+    private int number;
+    private String name;
+    private String position;
+    private String year;
+
+    public Player() { 
+        super();
+    }
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String toString() {   
+        return "#" + this.number + ": " + this.name + " (" + this.position + ", " + this.year + ")";
+    }
+
+    public Double computeScoringAverage(){
+        Random r = new Random();
+        double randomValue = 10.0*r.nextDouble();
+        return randomValue;
+    }
+
+
+    public int getNumber() {
+        return number;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getPosition() {
+        return position;
+    }
+    public String getYear() {
+        return year;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public void setPosition(String position) {
+        this.position = position;
+    }
+    public void setYear(String year) {
+        this.year = year;
+    }
 }
+
